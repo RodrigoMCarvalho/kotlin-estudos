0a2d85832c9dc1b841c4cad357ff2fcebca6fcb6
Rodrigo <rodrigo_moreira84@hotmail.com>
2021-08-10 20:19:32 -0300

    teste

--- bytebank/src/main.kt
+++ bytebank/src/main.kt
@@ -15,20 +15,24 @@ fun main() {
     conta2.numero = 1001
     conta2.saldo = 450.0
 
+    conta1.deposita( 50.0)
+    conta2.deposita(130.0)
+
     println(conta1.toString())
     println(conta2.toString())
 
+    conta1.saca( 150.0)
+    conta2.saca(500.0)
+
+    println(conta1.toString())
+    println(conta2.toString())
+
+
+
+
+
 }
 
-class Conta {
-    var titular = ""
-    var numero = 0
-    var saldo = 0.0
-    override fun toString(): String {
-        return "Conta(titular='$titular', numero=$numero, saldo=$saldo)"
-    }
-
-}
 
 
 private fun testarLacos() {
@@ -53,4 +57,30 @@ private fun testaCondicoes(saldo: Double) {
         saldo == 0.0 -> println("Conta neutra")
         else -> println("Conta negativa")
     }
+}
+
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+
+    fun deposita(valor: Double) {
+        println("Depositando na conta do $titular o valor de $valor")
+        saldo += valor
+    }
+
+    fun saca(valor: Double) {
+        when {
+            saldo < valor -> println("Saldo insuficiente na conta de $titular")
+            else -> {
+                println("Sacando na conta do $titular o valor de $valor")
+                saldo -= valor
+            }
+        }
+    }
+
+    override fun toString(): String {
+        return "Conta(titular='$titular', numero=$numero, saldo=$saldo)"
+    }
+
 }
