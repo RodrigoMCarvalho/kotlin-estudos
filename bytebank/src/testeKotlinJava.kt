10c9f0e5eb41f62acd2984f63338046c54251bec
Rodrigo <rodrigo_moreira84@hotmail.com>
2021-08-13 20:03:00 -0300

    testes

--- bytebank/src/testeKotlinJava.kt
+++ bytebank/src/testeKotlinJava.kt
@@ -1,14 +1,13 @@
-import java.util.*
 import java.util.function.Consumer
 import java.util.stream.Collectors
 
 fun main() {
-    val lista = Arrays.asList("Rodrigo", "Gustavo")
+    val lista = listOf("Rodrigo", "Gustavo")
     lista.stream().forEach { e: String? -> println(e) }
 
-    val usuarios = Arrays.asList(Usuario("Rodrigo", 30), Usuario("Gustavo", 7))
-    usuarios.stream().forEach { e: Usuario -> println(e.nome) }
+    val usuarios = listOf(Usuario("Rodrigo", 30), Usuario("Gustavo", 7))
+    usuarios.forEach { e: Usuario -> println(e.nome) }
 
     val maiorQueDez = usuarios.stream().filter { u: Usuario -> u.idade > 10 }.collect(Collectors.toList())
-    maiorQueDez.forEach(Consumer { x: Usuario? -> println(x?.idade) })
+    maiorQueDez.forEach { x: Usuario? -> println(x?.idade) }
 }
